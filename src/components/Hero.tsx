19e1519bc5fd1e5a40e1a232ef18e71b3454895d
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:11:26 +0100

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+// src/components/Hero.tsx
+import React from 'react';
+
+const Hero: React.FC = () => (
+  <section className="p-8 text-center bg-gray-800">
+    Hero Section
+  </section>
+);
+
+export default Hero;
