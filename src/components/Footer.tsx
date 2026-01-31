f62232c33576c073860753560d6bcedbb160bb24
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:14:23 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="p-4 bg-gray-900 text-white text-center">
+    Footer
+  </footer>
+);
+
+export default Footer;
