d151d59a15bb58a8f7da6a2e215a1611621c1963
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:13:33 +0100

    Create FAQ.tsx

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,10 @@
+// src/components/FAQ.tsx
+import React from 'react';
+
+const FAQ: React.FC = () => (
+  <section className="p-8 text-center bg-gray-300">
+    FAQ Section
+  </section>
+);
+
+export default FAQ;
