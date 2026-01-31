f2b417fa8bac419d6b734847578f6559f58ef82d
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:13:13 +0100

    Create StyleConsultant.tsx

---
+++ src/components/StyleConsultant.tsx
@@ -0,0 +1,10 @@
+// src/components/StyleConsultant.tsx
+import React from 'react';
+
+const StyleConsultant: React.FC = () => (
+  <section className="p-8 text-center bg-gray-400">
+    Style Consultant Section
+  </section>
+);
+
+export default StyleConsultant;
