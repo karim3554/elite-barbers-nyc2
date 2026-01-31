6a40aa559f38a0c8a92ed74e446749eebed49509
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:13:52 +0100

    Create LastCTA.tsx

---
+++ src/components/LastCTA.tsx
@@ -0,0 +1,10 @@
+// src/components/LastCTA.tsx
+import React from 'react';
+
+const LastCTA: React.FC = () => (
+  <section className="p-8 text-center bg-gray-200">
+    Last Call to Action Section
+  </section>
+);
+
+export default LastCTA;
