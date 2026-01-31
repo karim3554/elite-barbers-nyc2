5d1b403a2b073f6424ba6f10b032877fcf456aa9
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:12:55 +0100

    Create Gallery.tsx

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,10 @@
+// src/components/Gallery.tsx
+import React from 'react';
+
+const Gallery: React.FC = () => (
+  <section className="p-8 text-center bg-gray-500">
+    Gallery Section
+  </section>
+);
+
+export default Gallery;
