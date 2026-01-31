68e6f95a917d7d35e808375cf3d42c0295daed3c
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:12:28 +0100

    Create WhyUs.tsx

---
+++ src/components/WhyUs.tsx
@@ -0,0 +1,10 @@
+// src/components/WhyUs.tsx
+import React from 'react';
+
+const WhyUs: React.FC = () => (
+  <section className="p-8 text-center bg-gray-600">
+    Why Us Section
+  </section>
+);
+
+export default WhyUs;
