932b22cac7fce29e6aa4bb2c4f54085be5cea669
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:11:02 +0100

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+// src/components/Navbar.tsx
+import React from 'react';
+
+const Navbar: React.FC = () => (
+  <nav className="p-4 bg-gray-900 text-white text-center font-bold">
+    Navbar
+  </nav>
+);
+
+export default Navbar;
