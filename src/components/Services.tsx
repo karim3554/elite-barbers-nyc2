bd0497976ce1d50a841605fa2f33acf799c03b71
akram chouia <157923271+karim3554@users.noreply.github.com>
2026-01-31 12:12:07 +0100

    Create Services.tsx

---
+++ src/components/Services.tsx
@@ -0,0 +1,10 @@
+// src/components/Services.tsx
+import React from 'react';
+
+const Services: React.FC = () => (
+  <section className="p-8 text-center bg-gray-700">
+    Services Section
+  </section>
+);
+
+export default Services;
